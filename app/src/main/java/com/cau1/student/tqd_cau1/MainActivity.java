8ed31ff0c008f7c2e1ba6397e623f69ce1e25dab
H71M16 <dattruong999@gmail.com>
2019-09-09 08:17:06 +0700

    Thêm nút thoát cho trang đăng nhập

--- app/src/main/java/com/cau1/student/tqd_cau1/MainActivity.java
+++ app/src/main/java/com/cau1/student/tqd_cau1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cau1.student.tqd_cau1;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,9 +9,10 @@ import android.widget.Button;
 import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
-    Button bt_DangNhap;
+    Button bt_DangNhap,bt_Thoat;
     CheckBox ck_Luu;
     AlertDialog.Builder dialog;
+    AlertDialog.Builder dialog2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +34,29 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
+        bt_Thoat=(Button)findViewById(R.id.buttonThoat);
+        dialog2=new AlertDialog.Builder(this);
+        dialog2.setTitle("Thông báo");
+        bt_Thoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog2.setMessage("Bạn có thực sự muốn thoát?");
+                dialog2.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                dialog2.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                dialog2.show();
+            }
+
+        });
+
     }
 }
